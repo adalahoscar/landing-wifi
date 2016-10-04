5fba970f54e53e8ddb1a6cf38097fbe9a5517cb3
adalahoscar <adalahoscar@gmail.com>
2016-10-04 16:48:01 +0700

    init
    first upload file

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+ $('.carousel.carousel-slider').carousel({full_width: true});
